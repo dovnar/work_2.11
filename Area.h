c2e28cda91da3bba7ec7b36c832f9bd69ba39ad1
setlol <dovnar.maxim@gmail.com>
2016-12-07 09:06:19 +0300

    Update Area.h

--- Area.h
+++ Area.h
@@ -14,20 +14,21 @@ public:
 	static int getCountOfClass();
 	
 
+	
 	///Triangle ->
-	static float getTriangleArea(float basis, float height);	//вычисление площади по основанию и высоте 
-	static float getTriangleArea(float party1, float party2, float corner, bool flag);	//вычисление площади по двум сторонам и углу между ними = true
-	// float getTriangleArea(float party1, float party2, float party3);	//вычисление площади по трем сторонам = false
+	static float getTriangleArea(float basis, float height);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РѕСЃРЅРѕРІР°РЅРёСЋ Рё РІС‹СЃРѕС‚Рµ 
+	static float getTriangleArea(float party1, float party2, float corner, bool flag);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РґРІСѓРј СЃС‚РѕСЂРѕРЅР°Рј Рё СѓРіР»Сѓ РјРµР¶РґСѓ РЅРёРјРё = true
+	// float getTriangleArea(float party1, float party2, float party3);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ С‚СЂРµРј СЃС‚РѕСЂРѕРЅР°Рј = false
 	
 	///rectangle ->
-	static float getRectangleArea(float party1, float party2);	//вычисление площади по двум сторонам
+	static float getRectangleArea(float party1, float party2);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РґРІСѓРј СЃС‚РѕСЂРѕРЅР°Рј
 	///square ->
-	static float getSquareArea(float party);	//вычисление площади по одной стороне
-	static float getSquareArea(float party1, float party2);	//вычисление площади по двум сторонам	///parallelogram ->
-	static float getParallelogramArea(float party, float height);	//вычисление площади по одной стороне и высоте
-	static float getParallelogramArea(float party1, float party2, float corner);	//вычисление площади по двум сторонам и углу между ними
+	static float getSquareArea(float party);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РѕРґРЅРѕР№ СЃС‚РѕСЂРѕРЅРµ
+	static float getSquareArea(float party1, float party2);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РґРІСѓРј СЃС‚РѕСЂРѕРЅР°Рј
+	///parallelogram ->
+	static float getParallelogramArea(float party, float height);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РѕРґРЅРѕР№ СЃС‚РѕСЂРѕРЅРµ Рё РІС‹СЃРѕС‚Рµ
+	static float getParallelogramArea(float party1, float party2, float corner);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РґРІСѓРј СЃС‚РѕСЂРѕРЅР°Рј Рё СѓРіР»Сѓ РјРµР¶РґСѓ РЅРёРјРё
 	///rhombus ->
-	static float getRhombusArea(float party, float corner, bool flag);	//вычисление площади по стороне и углу = true
-	//static float getRhombusArea(float diagonal1, float diagonal2); //вычисление площади по двум диагоналям = false
+	static float getRhombusArea(float party, float corner, bool flag);	//РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ СЃС‚РѕСЂРѕРЅРµ Рё СѓРіР»Сѓ = true
+	//static float getRhombusArea(float diagonal1, float diagonal2); //РІС‹С‡РёСЃР»РµРЅРёРµ РїР»РѕС‰Р°РґРё РїРѕ РґРІСѓРј РґРёР°РіРѕРЅР°Р»СЏРј = false
 };
-
